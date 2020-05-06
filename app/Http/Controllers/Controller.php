e899d70dc51eae9b6dd86fbf143e8fb01e6403fe
DenisDorogov <denisdorogov@yandex.ru>
2020-05-06 21:40:53 +0500

    Добавлены методы getCategory и getNews. Возвращают массивы с данными.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,74 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function getCategory() {
+        $array = [
+            'sience',
+            'gatgets',
+            'sport',
+            'politics',
+            'nature'
+        ];
+        return $array;
+    }
+
+    public function getNews()
+    {
+        $array = [
+            'sience' =>
+                [
+                    [
+                        'title' => 'На экзопланетах с водородными атмосферами может существовать жизнь',
+                        'text' => ''
+                    ],
+                    [
+                        'title' => '',
+                        'text' => ''
+                    ],
+                    [
+                        'title' => '',
+                        'text' => ''
+                    ],
+                    [
+                        'title' => '',
+                        'text' => ''
+                    ]
+                ],
+            'gatgets' =>
+                [
+                    [
+                        'title' => '',
+                        'text' => ''
+                    ],
+                    [
+                        'title' => '',
+                        'text' => ''
+                    ],
+                    [
+                        'title' => '',
+                        'text' => ''
+                    ],
+                    [
+                        'title' => '',
+                        'text' => ''
+                    ]
+                ],
+            'sport' =>
+                [
+
+                ],
+            'politics' =>
+                [
+
+                ],
+            'nature' =>
+                [
+
+                ]
+        ];
+        return $array;
+    }
+
+
 }
