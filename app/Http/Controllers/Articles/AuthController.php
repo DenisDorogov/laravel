dd35c5ade6d04e9d2b6ea37b8b54a4b171dcb657
DenisDorogov <denisdorogov@yandex.ru>
2020-05-11 16:00:02 +0500

    Пара экшенов для аутентификации.

--- app/Http/Controllers/Articles/AuthController.php
+++ app/Http/Controllers/Articles/AuthController.php
@@ -7,5 +7,21 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function auth()
+    {
+        return view('articles/auth', [
+            'links' => $this->getLinks()
+        ]);
+    }
+
+    public function checkUser(Request $request)
+    {
+        $data = $request->all();
+        $login = $data['login'];
+        $pass = $data['password'];
+        $data['remember'] == 'on' ? $remember = true : $remember = false;
+        dd($data);
+
+        // return ;
+    }
 }
